fa52174482db4657865be7644602324eba5de93c
Maksym Topolya <148054104+MaksymTopolya@users.noreply.github.com>
2024-03-23 20:34:13 +0300

    main

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,7 +1,8 @@
- export function getPhotos(query) {
+
+export function getPhotos(query) {
+    startLoading()
   const BASE_URL = 'https://pixabay.com';
   const END_POINT = '/api/';
-     
   const params = new URLSearchParams({
     key: '43031538-631b2ceb9342d78106785df9b',
     q: query,
@@ -9,21 +10,21 @@
     orientation: 'horizontal',
     safesearch: true,
   });
-
   const url = `${BASE_URL}${END_POINT}?${params}`;
-     
-const options = {
-  headers: {
-    'X-RateLimit-Limit': 1000,
-    'X-RateLimit-Remaining': 50,
-    'X-RateLimit-Reset': 1800,
-    },
-};
-
-
-  return fetch(url, options)
+  return fetch(url)
     .then(res => res.json())
       .then(data => {
-      return data;
-    })
+          hideLoading()
+          return data; 
+      })
+}
+
+const loading = document.querySelector(".loading");
+
+function startLoading() {
+    loading.classList.remove("hide"); 
+}
+
+function hideLoading() {
+    loading.classList.add("hide"); 
 }
--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,22 +1,80 @@
-import { getPhotos } from "./pixabay-api";
+import { getPhotos } from './pixabay-api';
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
 
-const myForm = document.querySelector(".form")
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
+const myForm = document.querySelector(".form");
+const list = document.querySelector(".list");
+const loading = document.querySelector(".loading");
 
-myForm.addEventListener("submit", getImg)
-
+myForm.addEventListener("submit", getImg);
 
 function getImg(event) {
-    event.preventDefault()
-    const inputValue = event.target.elements.search.value
-    getPhotos(inputValue.toLowerCase()).then(photo => {
-         console.log(photo)
-     })
+    
+    event.preventDefault();
+    const inputValue = event.target.elements.search.value;
+    
+    getPhotos(inputValue.toLowerCase())
+        .then(response => {
+            const photos = response.hits; 
+            if (photos.length === 0) {
+               return  iziToast.error({
+                title: 'Hey',
+                message: `Sorry, there are no images matching your search query. Please try again!`,
+                   theme: 'dark',
+                backgroundColor: 'red',
+                position: 'topCenter',
+                     color: 'black', 
+            });
+            }
+            const markup = markupMyImg(photos);
+            list.innerHTML = markup;
+            initializeLightbox();
+        }).finally(() => {
+            event.target.reset();
+        });
+}
 
-    
-    
-    
-    
-    event.target.reset()
+function markupMyImages({webformatURL, largeImageURL, tags, likes, views, comments, downloads}) {
+    return `
+        <li class="list-item">
+            <a href="${largeImageURL}" class="photo" data-lightbox="photos">
+                <img src="${webformatURL}" class="photo" data-img="${largeImageURL}">
+            </a>
+            <div class="desc">
+                <ul class="info">
+                    <li class="mini-item">
+                        <h3 class="mini-title">Likes</h3>
+                        <p class="about-photo">${likes}</p>
+                    </li>
+                    <li class="mini-item">
+                        <h3 class="mini-title">Views</h3>
+                        <p class="about-photo">${views}</p>
+                    </li>
+                    <li class="mini-item">
+                        <h3 class="mini-title">Comments</h3>
+                        <p class="about-photo">${comments}</p>
+                    </li>
+                    <li class="mini-item">
+                        <h3 class="mini-title">Downloads</h3>
+                        <p class="about-photo">${downloads}</p>
+                    </li>
+                </ul>
+            </div>
+        </li>
+    `;
+}
+
+function markupMyImg(arr) {
+    return arr.map(markupMyImages).join('');
+}
+
+function initializeLightbox() {
+    const lightbox = new SimpleLightbox('.list-item a', {
+        captionsData: 'alt',
+        captionDelay: 250, 
+    });
 }
 
